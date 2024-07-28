d6e0c751254d330887369336be1826377e7d584b
Deepansh Srivastav <deepanshsrivastav9711@gmail.com>
2024-07-28 17:10:49 +0530

    Get all planets API is working

---
+++ Server/src/server.js
@@ -0,0 +1,20 @@
+const app = require("./app")
+
+const { loadPlanetsData } = require("./models/planets.model")
+
+const PORT = 8000
+
+const http = require("http")
+
+const server = http.createServer(app)
+
+async function startServer() {
+
+    await loadPlanetsData()
+
+    server.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}...`);
+    })
+}
+
+startServer()
